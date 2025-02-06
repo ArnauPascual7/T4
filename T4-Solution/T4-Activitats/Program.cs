1eaca1d1025ea9836fa3628f1eaeef1f496fc365
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-06 13:49:53 +0100

    Exercise 3 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,27 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string MsgName = "Introdueix el nom:";
+            const string MsgAge = "Introdueix l'edat:";
+            const string MsgRes = "Valors";
+
+            Dictionary<string, int> ages = new Dictionary<string, int>();
+            string name = "";
+            int age = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(MsgName);
+                name = Console.ReadLine();
+                Console.WriteLine(MsgAge);
+                age = Convert.ToInt32(Console.ReadLine());
+                ages.Add(name, age);
+            }
+
+            foreach (var item in ages)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
         }
     }
 }
