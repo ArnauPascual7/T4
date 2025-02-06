5e48fe1c163dee75855adfa62289330a58c00556
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-06 13:37:04 +0100

    Exercise 2 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace T4_Activitats
+﻿using System.Collections;
+
+namespace T4_Activitats
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Introdueix un valor:";
+            const string MsgRes = "Valors:";
+
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(Msg);
+                arrayList.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(MsgRes);
+            foreach (string item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
