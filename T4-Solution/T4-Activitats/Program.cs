a5137ecb920a77c2583ba369085d5253e5904318
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-06 13:24:33 +0100

    Exercise 1 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Parella<string, int> parella1 = new Parella<string, int>("Hola", 1);
+            Parella<double, bool> parella2 = new Parella<double, bool>(3.5d, true);
+
+            parella1.ShowItems();
+            parella2.ShowItems();
         }
     }
 }
