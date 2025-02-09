b3505a163d51eba6509f784e783c6dcf3969bf4d
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-09 12:04:51 +0100

    Exercise 5 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,22 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Introdueix un número:";
+            const string MsgResult = "Nombres parells:";
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(Msg);
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.WriteLine(MsgResult);
+            foreach (int i in list)
+            {
+                if (i % 2 == 0) Console.WriteLine(i);
+            }
         }
     }
 }
