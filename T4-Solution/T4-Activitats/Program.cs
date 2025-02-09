c6abdf6dc96f3cdadb1e83743537631d3e785c0f
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-09 11:55:11 +0100

    Exercise 4 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,33 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Introdueix un nom:";
+            const string MsgResult = "Llista:";
+            const string MsgOrderedResult = "Llista ordenada:";
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Msg);
+                list.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(MsgResult);
+            foreach (string name in list)
+            {
+                Console.WriteLine(name);
+            }
+
+            list.Sort();
+
+            Console.WriteLine();
+            Console.WriteLine(MsgOrderedResult);
+            foreach (string name in list)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
