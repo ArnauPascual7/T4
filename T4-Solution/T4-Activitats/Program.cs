106ddedcf6037972bcce20cf541de2b033c85124
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-12 12:20:30 +0100

    Exercise 23 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -2,9 +2,13 @@
 {
     public class Program
     {
+        public delegate int Operation(int a, int b);
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Operation power = (a, b) => (int)Math.Pow(a, b);
+            Func<int, int, double> pow = (a, b) => Math.Pow(a, b);
+            Console.WriteLine(power(2, 2));
+            Console.WriteLine(pow(2, 3));
         }
     }
 }
