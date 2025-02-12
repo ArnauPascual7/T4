815b65bc944a2433b741f9e4c6d27ca843f7aeb0
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-12 12:06:28 +0100

    Exercise 20 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -2,9 +2,14 @@
 {
     public class Program
     {
+        public delegate int Operation(int a, int b);
+        public static int Sum(int a, int b) => a + b;
+        public static void ExecuteOperation(int a, int b, Operation op) => Console.WriteLine(op(a, b));
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ExecuteOperation(10, 2, (a, b) => a * b);
+            ExecuteOperation(10, 2, (a, b) => a - b);
+            ExecuteOperation(10, 2, Sum);
         }
     }
 }
