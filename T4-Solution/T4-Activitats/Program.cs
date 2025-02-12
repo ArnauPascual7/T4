5489387551b39e3cc1967b868a95ac6a76a87c08
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-12 11:17:13 +0100

    Exercise 6 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Introdueix una data:";
+            const string MsgRes = "Dies restants: {0}";
+            const string Err = "Data incorrecte";
+
+            DateTime dateTime;
+            Console.WriteLine(Msg);
+            // dateTime = DateTime.Parse(Console.ReadLine());
+            while (!DateTime.TryParse(Console.ReadLine(), out dateTime))
+            {
+                Console.WriteLine(Err);
+            }
+
+            Console.WriteLine(MsgRes, (dateTime - DateTime.Today).Days);
         }
     }
 }
