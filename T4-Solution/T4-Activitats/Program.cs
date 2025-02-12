d78084dc5b450d0fdace317e6b1822975230530f
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-12 11:43:44 +0100

    Exercise 19 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace T4_Activitats
+﻿using System;
+
+namespace T4_Activitats
 {
     public class Program
     {
+        public delegate int Operation(int a, int b);
+        public static int Multiply(int a, int b) => a * b;
+        public static int Divide(int a, int b) => a / b;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Operation multiply = Multiply;
+            Operation divide = Divide;
+            //Operation multiply = (a, b) => a * b;
+            //Operation divide = (a, b) => a / b;
+
+            Console.WriteLine(multiply(3, 2));
+            Console.WriteLine(divide(10, 2));
         }
     }
 }
