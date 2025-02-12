ce58b8ce9519163e0ffb6b322462c02bc89625b1
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-12 13:07:20 +0100

    Exercise 24 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -2,9 +2,11 @@
 {
     public class Program
     {
+        public delegate void Notification(string msg);
+        public static void ExecutarAmbMetodeAnonim(Notification notification) => notification("a");
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ExecutarAmbMetodeAnonim(delegate (string msg) { Console.WriteLine(msg); } );
         }
     }
 }
