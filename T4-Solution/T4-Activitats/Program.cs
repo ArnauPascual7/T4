61ac6984ab8c66f7fff0db178adbf74ec1449bfd
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-02-16 23:01:20 +0100

    Exercise 10 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,17 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(IsSecondDatePosterior());
+        }
+        public static bool IsSecondDatePosterior()
+        {
+            Console.WriteLine("Introdueix dues dates (dd, mm, yyyy - dd, mm, yyyy):");
+            string[] array = Console.ReadLine().Split("-");
+
+            DateTime dt1 = DateTime.Parse(array[0]);
+            DateTime dt2 = DateTime.Parse(array[1]);
+
+            return dt1 < dt2;
         }
     }
 }
