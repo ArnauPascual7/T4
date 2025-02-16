572c2f076c1ffedb5ed503c7091e70d7fb0f592f
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-02-16 21:03:55 +0100

    Exercise 8 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> list = [5, 10, 15, 20, 25];
+            list.Add(30);
+            list.Insert(1, 7);
+            list.Remove(15);
+            list.Reverse();
+
+            foreach (int item in list)
+            {
+                if (item % 2 == 0) Console.WriteLine(item);
+            }
         }
     }
 }
