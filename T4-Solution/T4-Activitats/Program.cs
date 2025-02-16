fb9bc4ce4a65c6b58ebcaeae4c1b8e7e0b983d2c
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-02-16 22:51:26 +0100

    Exercise 9 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,28 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            students.Add("Marc", 21);
+            students.Add("Laura", 19);
+            students.Add("Pau", 22);
+
+            Console.WriteLine("Diccionari");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key}: {student.Value}");
+            }
+
+            Console.WriteLine("Introdueix el nom d'un estudiant:");
+            string name = Console.ReadLine();
+            Console.WriteLine(students.ContainsKey(name) ? $"L'estudiant existeix: {name} {students[name]}" : "L'estudiant no existeix");
+
+            students.Remove("Laura");
+
+            Console.WriteLine("Diccionari sense Laura");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key}: {student.Value}");
+            }
         }
     }
 }
