133af194bb663ab20c090658ec93e3eac742b83f
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-02-16 20:48:29 +0100

    Exercise 7 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace T4_Activitats
+﻿using System.Collections;
+
+namespace T4_Activitats
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("Maria");
+            arrayList.Add("Joan");
+            arrayList.Add("Anna");
+            arrayList.Add(42);
+            arrayList.Add(true);
+
+            Console.WriteLine("Arraylist");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrayList.Remove(42);
+            arrayList.Remove(true);
+            arrayList.Insert(2, "Pere");
+
+            Console.WriteLine("Anna està en la llista? " + arrayList.Contains("Anna"));
+
+            string[] array = (string[])arrayList.ToArray(typeof(string));
+
+            Console.WriteLine("Array de Strings");
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
