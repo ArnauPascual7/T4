2c851fa747efc978c430c998e4b7bca83c49dd82
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-02-18 10:07:12 +0100

    Xml Project Created

---
+++ T4-Solution/XmlProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace XmlProject
+{
+    public class Program
+    {
+        private const string File = "book.xml";
+        private const string Path = @"..\..\..\Files\" + File;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
