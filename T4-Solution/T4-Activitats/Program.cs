f2efc72491033298ad7a9d031c2de6392626f022
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-02-23 20:38:46 +0100

    Exercise 17 Done

--- T4-Solution/T4-Activitats/Program.cs
+++ T4-Solution/T4-Activitats/Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, float> employees = new Dictionary<string, float>();
+            employees.Add("A", 1000.99f);
+            employees.Add("B", 2222.22f);
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.Key}: {employee.Value}");
+            }
         }
     }
 }
